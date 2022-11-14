019eee738428198d707151975993abbde5c69340
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2022-11-14 08:36:29 +0100

    feat( setup ): initial setup

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <PageHeader />
+  <Home />
+  <AuthModal />
+</template>
+
+<script lang="ts">
+import PageHeader from "@/components/header.vue";
+import AuthModal from "@/components/auth-modal.vue";
+import Home from "@/views/home.vue";
+
+export default {
+  name: "App",
+  components: {
+    Home,
+    PageHeader,
+    AuthModal,
+  },
+};
+</script>
---
+++ src/views/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <Introduction />
+  <HomeContent />
+  <Player />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import Introduction from "@/components/home/introduction.vue";
+import Player from "@/components/home/player.vue";
+import HomeContent from "@/components/home/content.vue";
+
+export default defineComponent({
+  name: "home-page",
+  components: {
+    Introduction,
+    Player,
+    HomeContent,
+  },
+});
+</script>
